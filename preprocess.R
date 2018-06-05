6ad5fd9ea300a537edc8ba02ebbad90fddd4e108
Raul Carlomagno <rcarlomagno@gmail.com>
2018-06-05 10:12:51 -0300

    explorando

--- preprocess.R
+++ preprocess.R
@@ -1,15 +1,40 @@
+libs <- c("data.table", "DataExplorer")
+sapply(libs[sapply(libs, require, character.only = TRUE, quietly = TRUE) == FALSE], install.packages)
+
+#lubripack
+sapply(libs, require, character.only = TRUE)
+
+library(data.table)
+library(DataExplorer)
+
+libs <- c("data.table", "DataExplorer")
+sapply(libs[sapply(libs, require, character.only = TRUE, quietly = TRUE) == FALSE], install.packages)
+
 (if(!require("data.table", quietly = TRUE)) install.packages("data.table"))
 (if(!require("ggplot2", quietly = TRUE)) install.packages("ggplot2"))
-library(scales)
+(if(!require("DataExplorer", quietly = TRUE)) install.packages("DataExplorer"))
 
 csvData <- fread("gender-classifier.csv")
 csvData$tweet_id <- NULL
 csvData$profileimage <- NULL
+csvData$gender_gold <- NULL
+csvData$profile_yn_gold <- NULL
+csvData$'_unit_id' <- NULL
+
+
 
 str(csvData)
+plot_missing(csvData)
+plot_histogram(csvData)
+plot_bar(csvData)
+create_report(csvData)
 
+summary(csvData$profile_yn)
 
 unique(csvData$gender_gold)
 qplot(csvData$gender_gold)
+
+table(csvData$profile_yn_gold)
+table(csvData$profile_yn_gold) / sum(table(csvData$profile_yn_gold))
 
 
