c78b98e09eac545c80604c72348fb37c74b16021
Raul Carlomagno <rcarlomagno@gmail.com>
2018-06-12 18:10:59 -0300

    avance

--- preprocess.R
+++ preprocess.R
@@ -8,12 +8,13 @@ library("tokenizers")
 #library("tm") #detach("package:tm", unload = TRUE)
 library(plyr)
 library("stringr")
+#library("fastrtext")
 
 stopwords <- scan("stopwords.txt", character(), quote = "")
 stopwords <- c(stopwords, scan("customstopwords.txt", character(), quote = ""))
 stopwords <- c(stopwords, as.character(0:10))
 
-csvData <- fread("gender-classifier.csv")
+csvData <- fread("gender-classifier.csv", encoding = 'UTF-8')
 csvData$tweet_id <- NULL
 csvData$profileimage <- NULL
 csvData$gender_gold <- NULL
@@ -22,26 +23,31 @@ csvData$'_unit_id' <- NULL
 csvData <- csvData[csvData$profile_yn == "yes",]
 csvData$profile_yn <- NULL
 csvData <- csvData[csvData$gender != "unknown",]
+csvData$gender <- as.factor(csvData$gender)
 csvData$'_unit_state' <- NULL
 csvData$diff_prof_twt <- as.numeric(difftime(mdy_hms(csvData$tweet_created), mdy_hms(csvData$created), units = c("weeks")))
 csvData$tweet_created <- NULL
 csvData$created <- NULL
 csvData$`_last_judgment_at` <- NULL
 csvData$name <- NULL
+csvData$tweet_coord <- NULL
+csvData$tweet_location <- NULL
 csvData$`_golden` <- NULL
 csvData$`_trusted_judgments` <- NULL
 csvData$text <- str_replace_all(csvData$text, "&amp;", "&");
 
-# ver lo de los colores
-colores <- paste0("#",subset(csvData$sidebar_color, nchar(csvData$sidebar_color) == 6  )  )
-# coloresrgb <- col2rgb(colores)[]
-# coloresint <- coloresrgb[1,] * 256 * 256 + coloresrgb[2,] * 256 + coloresrgb[3,]
-# 256*256*red+256*green+blue
-# qplot(coloresint)
-# qplot( discretize(coloresint) )
+
+csvData[nchar(csvData$sidebar_color) != 6, "sidebar_color"] <- "ZZZZZZ" #seteo un color cuando no tienen un color valido
+csvData$sidebar_color <- paste0("#", csvData$sidebar_color)
 
 #https://i.imgur.com/PKjgfFXm.jpg
+#mejorar
+#hay mucho cyan por #C0DEED
+#ver q onda los blancos , negros y que color setear cuando no tiene uno valido
 hex2colorname <- function(hexacolor){
+  if(hexacolor == "#ZZZZZZ")
+    return("indeterminado")  
+  
   coloresrgb <- col2rgb(hexacolor)[]
   hue <- rgb2hsv(coloresrgb[1,],coloresrgb[2,],coloresrgb[3,])["h",1] * 360
   
@@ -60,31 +66,53 @@ hex2colorname <- function(hexacolor){
   if(hue >= 270 & hue < 285)
     return("violet")
   if(hue >= 285 & hue < 345)
-    return("violet")
+    return("magenta")
 }
 
+csvData$sidebar_color_cat <- sapply(csvData$sidebar_color, hex2colorname)
+ggplot(csvData, aes(sidebar_color_cat)) +
+  geom_bar(aes(fill = gender), position=position_dodge())
 
 
-qplot(tokenize_word_stems(csvData$text, stopwords = stopwords::stopwords("en")))
-
-unique(tokenize_words(csvData$text, stopwords = stopwords::stopwords("en")))
 
 
-procesarPalabras <- function (genero){
+
+
+
+armarVocabularios <- function (genero){
   tokens <- tokenize_tweets(csvData[csvData$gender == genero,]$text, stopwords = stopwords)
   tokens <- unlist(tokens)
   tokens <- tokens[is.na(as.numeric(tokens))] #filtro los numeros
-  tokens <- tokens[str_length(tokens) > 2]
-    
+  tokens <- tokens[!grepl("www.|http:|https:", tokens)] #filtro urls
+  tokens <- tokens[str_length(tokens) > 2] #filtro palabras menores a 3
+  
   dftokens <- count(tokens)
-  return(head(dftokens[order(-dftokens$freq),], 30)[,1])
+  dftokensMost <- head(dftokens[order(-dftokens$freq),], 50)
+  dftokensMost$relative <- dftokensMost$freq / sum(dftokensMost$freq) * 100
+  dftokensMost$freq <- NULL
+  
+  return(dftokensMost)
 }
 
-listas <- sapply(unique(csvData$gender), procesarPalabras)
-listas
+vocabularios <- lapply(unique(csvData$gender), armarVocabularios)
+vocabularios.male <- vocabularios[[1]]
+vocabularios.female <- vocabularios[[2]]
+vocabularios.brand <- vocabularios[[3]]
 
 
 
+csvData$score_vocab_male <- 
+
+match(tokenize_tweets(csvData$text, stopwords = stopwords)[[1]], vocabularios.male)
+match(tokenize_tweets(csvData$text, stopwords = stopwords)[[1]], vocabularios.female)
+match(tokenize_tweets(csvData$text, stopwords = stopwords)[[1]], vocabularios.brand)
+
+tokens <- tokenize_tweets(csvData$text, stopwords = stopwords)
+tokens[[1000]]
+
+match(tokens[[1000]], vocabularios.male$x)
+
+  
 str(csvData)
 
 plot_missing(csvData)
