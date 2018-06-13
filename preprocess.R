d7cf9d15a2cc92565a6d6a791c479387a4523d20
Raul Carlomagno <rcarlomagno@gmail.com>
2018-06-13 17:28:23 -0300

    casi finalizado el procesamiento

--- preprocess.R
+++ preprocess.R
@@ -19,6 +19,7 @@ csvData$tweet_id <- NULL
 csvData$profileimage <- NULL
 csvData$gender_gold <- NULL
 csvData$profile_yn_gold <- NULL
+csvData$user_id <- csvData$'_unit_id'
 csvData$'_unit_id' <- NULL
 csvData <- csvData[csvData$profile_yn == "yes",]
 csvData$profile_yn <- NULL
@@ -36,17 +37,24 @@ csvData$tweet_location <- NULL
 csvData$`_golden` <- NULL
 csvData$`_trusted_judgments` <- NULL
 csvData$text <- str_replace_all(csvData$text, "&amp;", "&");
+csvData$gender_confidence <- csvData$`gender:confidence`
+csvData$`gender:confidence` <- NULL
+csvData$profile_yn_confidence <- csvData$`profile_yn:confidence`
+csvData$`profile_yn:confidence` <- NULL
+csvData$user_timezone <- NULL
+csvData[nchar(csvData$sidebar_color) != 6, "sidebar_color"] <- NA #seteo NA cuando no tienen un color valido
+csvData$sidebar_color[!is.na(csvData$sidebar_color)] <- paste0("#", csvData$sidebar_color[!is.na(csvData$sidebar_color)])
+csvData[nchar(csvData$link_color) != 6, "link_color"] <- NA #seteo NA cuando no tienen un color valido
+csvData$link_color[grep("\\+", csvData$link_color)] <- NA #unos tenian datos raros
+csvData$link_color[!is.na(csvData$link_color)] <- paste0("#", csvData$link_color[!is.na(csvData$link_color)])
 
-
-csvData[nchar(csvData$sidebar_color) != 6, "sidebar_color"] <- "ZZZZZZ" #seteo un color cuando no tienen un color valido
-csvData$sidebar_color <- paste0("#", csvData$sidebar_color)
 
 #https://i.imgur.com/PKjgfFXm.jpg
 #mejorar
 #hay mucho cyan por #C0DEED
 #ver q onda los blancos , negros y que color setear cuando no tiene uno valido
 hex2colorname <- function(hexacolor){
-  if(hexacolor == "#ZZZZZZ")
+  if(is.na(hexacolor))
     return("indeterminado")  
   
   coloresrgb <- col2rgb(hexacolor)[]
@@ -74,11 +82,16 @@ csvData$sidebar_color_cat <- sapply(csvData$sidebar_color, hex2colorname)
 ggplot(csvData, aes(sidebar_color_cat)) +
   geom_bar(aes(fill = gender), position=position_dodge())
 
+csvData$link_color_cat <- sapply(csvData$link_color, hex2colorname)
+ggplot(csvData, aes(link_color_cat)) +
+  geom_bar(aes(fill = gender), position=position_dodge())
 
 
 
-armarVocabularios <- function (genero){
-  tokens <- tokenize_tweets(csvData[csvData$gender == genero,]$text, stopwords = stopwords)
+
+
+armarVocabularios <- function (genero,columnName){
+  tokens <- tokenize_tweets(csvData[csvData$gender == genero, columnName, with=FALSE][[columnName]], stopwords = stopwords)
   tokens <- unlist(tokens)
   tokens <- tokens[is.na(as.numeric(tokens))] #filtro los numeros
   tokens <- tokens[!grepl("www.|http:|https:", tokens)] #filtro urls
@@ -93,10 +106,15 @@ armarVocabularios <- function (genero){
   return(dftokensMost)
 }
 
-vocabularios <- lapply(unique(csvData$gender), armarVocabularios)
+vocabularios <- lapply(unique(csvData$gender), armarVocabularios, columnName="text")
 vocabularios.male <- vocabularios[[1]]
 vocabularios.female <- vocabularios[[2]]
 vocabularios.brand <- vocabularios[[3]]
+
+vocabulariosBio <- lapply(unique(csvData$gender), armarVocabularios, columnName="description")
+vocabulariosBio.male <- vocabulariosBio[[1]]
+vocabulariosBio.female <- vocabulariosBio[[2]]
+vocabulariosBio.brand <- vocabulariosBio[[3]]
 
 #tmp <- csvData[order(csvData$gender,csvData$diff_prof_twt),]
 tmp <- csvData[order(csvData$gender),]
@@ -104,7 +122,7 @@ ggplot(data=tmp,aes(x=seq_along(tmp$diff_prof_twt), y=diff_prof_twt, color=gende
   geom_point( )
 
 
-tmp2 <- csvData[order(csvData$gender, csvData),]
+tmp2 <- csvData[order(csvData$gender),]
 ggplot(data=tmp2,aes(x=seq_along(tmp2$tweets_per_day), y=tweets_per_day, color=gender)) +
   geom_point( )
 
@@ -119,6 +137,11 @@ csvData$score_vocab_brand <- sapply(tweets_tokens, calculateVocabularyScore, voc
 csvData$score_vocab_male <- sapply(tweets_tokens, calculateVocabularyScore, vocabulary = vocabularios.male)
 csvData$score_vocab_female <- sapply(tweets_tokens, calculateVocabularyScore, vocabulary = vocabularios.female)
 
+description_tokens <- tokenize_tweets(csvData$description, stopwords = stopwords)
+csvData$score_vocabio_brand <- sapply(description_tokens, calculateVocabularyScore, vocabulary = vocabulariosBio.brand)
+csvData$score_vocabio_male <- sapply(description_tokens, calculateVocabularyScore, vocabulary = vocabulariosBio.male)
+csvData$score_vocabio_female <- sapply(description_tokens, calculateVocabularyScore, vocabulary = vocabulariosBio.female)
+
 predictOnScore <- function(score_vocab_brand, score_vocab_male, score_vocab_female){
   if(score_vocab_brand > score_vocab_male & score_vocab_brand > score_vocab_female)
     return("brand")
@@ -129,28 +152,23 @@ predictOnScore <- function(score_vocab_brand, score_vocab_male, score_vocab_fema
   
   return(NA)
 }
-gender_predicts <- mapply(predictOnScore, csvData$score_vocab_brand,csvData$score_vocab_male,csvData$score_vocab_female)
-pred <- data.frame(gender_predicts,csvData$gender)
-pred <- pred[!is.na(pred$gender_predicts),]
-table(pred)
-mean(pred$gender_predicts == pred$csvData.gender)
+
+#gender_predicts <- mapply(predictOnScore, csvData$score_vocab_brand,csvData$score_vocab_male,csvData$score_vocab_female)
+#pred <- data.frame(gender_predicts,csvData$gender)
+#pred <- pred[!is.na(pred$gender_predicts),]
+#table(pred)
+#mean(pred$gender_predicts == pred$csvData.gender)
+#na.omit(gender_predicts == csvData$gender)
 
 
-na.omit(gender_predicts == csvData$gender)
 
 
+csvData$sidebar_color <- NULL
+csvData$link_color <- NULL
+csvData$description <- NULL
+csvData$text <- NULL
 
-match(tokenstext[[]], vocabularios.male)
 
-match(tokenize_tweets(csvData$text, stopwords = stopwords)[[1]], vocabularios.female)
-match(tokenize_tweets(csvData$text, stopwords = stopwords)[[1]], vocabularios.brand)
-
-tokens <- tokenize_tweets(csvData$text, stopwords = stopwords)
-tokens[[1000]]
-
-match(tokens[[1000]], vocabularios.male$x)
-
-  
 str(csvData)
 
 plot_missing(csvData)
@@ -164,8 +182,17 @@ summary(csvData$profile_yn)
 unique(csvData$gender_gold)
 qplot(csvData$gender_gold)
 
-table(csvData$profile_yn_gold)
+table(csvData$'profile_yn:confidence')
 table(csvData$profile_yn_gold) / sum(table(csvData$profile_yn_gold))
 
 
 
+set.seed(unclass(Sys.time()))
+ids <- sample(nrow(csvData), replace = FALSE)
+idsScore <- sample(length(ids), 400, replace = FALSE)
+ids <- setdiff(ids, idsScore) #saco los id para scoring
+idsTrain <- sample(length(ids), round(0.8 * length(ids)), replace = FALSE)
+
+fwrite(csvData[idsScore,-c("gender")], "data-score.csv", quote = TRUE)
+fwrite(csvData[idsTrain,-c("user_id")], "data-train.csv", quote = TRUE)
+fwrite(csvData[-idsTrain,-c("user_id")], "data-test.csv", quote = TRUE)
