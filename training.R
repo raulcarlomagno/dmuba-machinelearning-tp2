b4e259ff61a2eb6c1bb6a4194d8a73b302baa6a5
Raul Carlomagno <rcarlomagno@gmail.com>
2018-06-29 01:25:17 -0300

    curvas casi terminadas

--- training.R
+++ training.R
@@ -1,35 +1,25 @@
 library("data.table")
 library("dplyr")
 library("mlr")
+library("pROC")
+library("plotROC")
 
 data <- fread("data.csv", data.table = FALSE, stringsAsFactors = TRUE)
-
-#por ahora hago clasificacion binaria, cuando sea multiclass,  poner stringasfactors = TRUE en fread
-#data[data$gender == 'male' | data$gender == 'female', 'gender'] <- 'human'
-#data$sidebar_color_cat <- as.factor(data$sidebar_color_cat)
-#data$link_color_cat <- as.factor(data$link_color_cat)
-
 glimpse(data)
 
 train.set <- sample(nrow(data), size = round(0.8 * nrow(data)), replace = FALSE)
 test.set <- setdiff(seq_len(nrow(data)), train.set)
 
-#defino la tarea de clasifiacion, hacerla multiclass despues
 task = makeClassifTask(id = "task1", data = data, target = "gender")
 print(task)
 
-#measures disponibles
-#https://mlr-org.github.io/mlr-tutorial/release/html/measures/index.html
-#https://www.math.ucdavis.edu/~saito/data/roc/ferri-class-perf-metrics.pdf
-#fpr (False positive rate): Percentage of misclassified observations in the positive class
-#tpr (True positive rate): Percentage of correctly classified observations in the positive class
-#mmce (Mean misclassification error): Defined as: mean(response != truth)
-#acc (Accuracy): Defined as: mean(response == truth)
 
 #https://mlr-org.github.io/mlr-tutorial/release/html/integrated_learners/
 # #ksvmLrnr o ksvmLrnr2
 learnerClasses <- c(
 #  "classif.randomForest",
+  "classif.glmnet",
+  "classif.multinom",
   "classif.kknn",
   "classif.rpart",
   "classif.lda",
@@ -57,45 +47,68 @@ bmr
 #df <- generateThreshVsPerfData(bmr, measures = list(fpr, tpr, mmce))
 #plotROCCurves(df)
 
-plot.roc(ensemblePred$data$truth, as.numeric(ensemblePred$data$response), levels=as.factor(c("brand", "male")))
 
 
 #creamos el modelo ensamblado
 stackedLearner <- makeStackedLearner(base.learners = learners, predict.type = "prob", method = "hill.climb")
 ensembleModel = train(stackedLearner, task, subset = train.set)
 ensemblePred = predict(ensembleModel, task, subset = test.set)
-ensemblePerf <- performance(ensemblePred, measures = list(acc, mmce))
+
+ensemblePerf <- performance(ensemblePred, measures = list(acc, mmce, multiclass.aunu))
 ensemblePerf
+#measureMMCE(ensemblePred$data$truth, ensemblePred$data$response)
+#measureACC(ensemblePred$data$truth, ensemblePred$data$response)
 
+aucBrand <- measureAUC(probabilities = ensemblePred$data$prob.brand, truth = ensemblePred$data$truth,  positive = "brand")
+aucMale <- measureAUC(probabilities = ensemblePred$data$prob.male, truth = ensemblePred$data$truth,  positive = "male")
+aucFemale <- measureAUC(probabilities = ensemblePred$data$prob.female, truth = ensemblePred$data$truth,  positive = "female")
+aucAvg <- ((aucBrand + aucMale + aucFemale) / 3)
 
-measureMMCE(ensemblePred$data$truth, ensemblePred$data$response)
-measureACC(ensemblePred$data$truth, ensemblePred$data$response)
-measureAUC(probabilities = ensemblePred$data$prob.brand, truth = ensemblePred$data$truth,  positive = "brand")
-measureAUC(probabilities = ensemblePred$data$prob.male, truth = ensemblePred$data$truth,  positive = "male")
-measureAUC(probabilities = ensemblePred$data$prob.female, truth = ensemblePred$data$truth,  positive = "female")
-
-library(pROC)
-prueba <- multiclass.roc(ensemblePred$data$truth, as.numeric(ensemblePred$data$response))
-prueba
-#plot.roc(ensemblePred$data$truth, as.numeric(ensemblePred$data$response), levels=as.factor(c("brand", "male")))
-#plot.roc(ensemblePred$data$truth, as.numeric(ensemblePred$data$response), levels=as.factor(c("brand", "female")))
-#plot.roc(ensemblePred$data$truth, as.numeric(ensemblePred$data$response), levels=as.factor(c("male", "female")))
 
 brandRoc <- roc(ifelse(ensemblePred$data$truth == 'brand', 'brand', 'other'), ensemblePred$data$prob.brand)
 brandRoc
-plot.roc(brandRoc, main="Brand vs others")
+dfRocBrand <- data.frame(value = ifelse(ensemblePred$data$truth == 'brand', 1, 0), prob = ensemblePred$data$prob.brand)
+ggplot(dfRocBrand, aes(d = value, m = prob)) +
+  ggtitle("Brand vs (Male & Female)") +
+  annotate("text", x = .75, y = .25, label = paste("AUC =", round(brandRoc$auc, 2))) +
+  geom_roc(n.cuts = 0) +
+  style_roc()
+
 
 maleRoc <- roc(ifelse(ensemblePred$data$truth == 'male', 'male', 'other'), ensemblePred$data$prob.male)
 maleRoc
-plot.roc(maleRoc, main="Male vs others")
+dfRocMale <- data.frame(value = ifelse(ensemblePred$data$truth == 'male', 1, 0), prob = ensemblePred$data$prob.male)
+ggplot(dfRocMale, aes(d = value, m = prob)) +
+  ggtitle("Male vs (Brand & Female)") +
+  annotate("text", x = .75, y = .25, label = paste("AUC =", round(maleRoc$auc, 2))) +
+  geom_roc(n.cuts = 0) +
+  style_roc()
+
 
 femaleRoc <- roc(ifelse(ensemblePred$data$truth == 'female', 'female', 'other'), ensemblePred$data$prob.female)
 femaleRoc
-plot.roc(femaleRoc, main="Male vs others")
+dfRocFemale <- data.frame(value = ifelse(ensemblePred$data$truth == 'female', 1, 0), prob = ensemblePred$data$prob.female)
+ggplot(dfRocFemale, aes(d = value, m = prob)) +
+  ggtitle("Female vs (Brand & Male)") +
+  annotate("text", x = .75, y = .25, label = paste("AUC =", round(femaleRoc$auc, 2))) +
+  geom_roc(n.cuts = 0) +
+  style_roc()
 
+
+dfRocAll <- rbind(cbind(dfRocBrand, class = "brand"), cbind(dfRocMale, class = "male"), cbind(dfRocFemale, class = "female"))
+ggplot(dfRocAll, aes(d = value, m = prob, color = class)) +
+  ggtitle("Brand vs Male vs Female") +
+  annotate("text", x = .75, y = .15, label = paste("Avg. AUC =", round(aucAvg, 2))) +
+  annotate("text", x = .75, y = .45, label = paste("Brand AUC =", round(brandRoc$auc, 2))) +
+  annotate("text", x = .75, y = .35, label = paste("Male AUC =", round(maleRoc$auc, 2))) +
+  annotate("text", x = .75, y = .25, label = paste("Female AUC =", round(femaleRoc$auc, 2))) +
+  geom_roc(n.cuts = 0) +
+  style_roc()
 
 
 #https://github.com/PhilippPro/MulticlassAUC/blob/master/MulticlassAUC.R
+#library(caTools)
+#colAUC(getPredictionProbabilities(ensemblePred), ensemblePred$data$truth, plotROC=TRUE)
 
-library(caTools)
-colAUC(getPredictionProbabilities(ensemblePred), ensemblePred$data$truth, plotROC=TRUE)
+
+
